96d2d8e735b7f3b68ed44529b98edeee3d306ce0
petrovp <petrov.petar.kav@gmail.com>
2024-05-15 17:43:12 +0200

    Kreirana tabela vo baza
    #6

---
+++ CodexMVCProject/DataLayer/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using CodexMVCProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodexMVCProject.DataLayer;
+
+public class ApplicationDbContext : DbContext
+{
+    protected readonly IConfiguration Configuration;
+
+    public ApplicationDbContext(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+           /* optionsBuilder.UseNpgsql("Server=abul.db.elephantsql.com;Port=5432;Database=eozukkfz;Username=eozukkfz;Password=2Bt3O5UFqVAIfDTUibtyk0o7hgBZOJtA");*/
+           optionsBuilder.UseNpgsql(Configuration.GetConnectionString("WebApiDatabase"));
+
+        }
+    }
+
+    public DbSet<Product> Products { get; set; }
+}
---
+++ CodexMVCProject/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace CodexMVCProject.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Naziv { get; set; } = null!;
+    public string Opis { get; set; } = null!;
+    public int Kolicina { get; set; } 
+    public int Iznos { get; set; }
+    public DateTime Datum { get; set; }
+
+}
