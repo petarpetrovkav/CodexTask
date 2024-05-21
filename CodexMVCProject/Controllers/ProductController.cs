a474782128c636c9f0a91f51bbf1a52b24546a98
petrovp <petrov.petar.kav@gmail.com>
2024-05-21 12:02:12 +0200

    Креирање на PARTIAL VIEW  за табеларен приказ на податоци од табела (табеларниот приказ може да е и готова компонента). Вчитување на PARTIAL VIEW со JQUERY без повторно вчитување на цела страна. Додавање на филтер на страната и при промена на вредноста да се промене приказот на податоците во табелата според филтерот по едно поле (пр. на назив). Внесување, ажурирање и бришење на податоци од табеларниот приказ.

--- CodexMVCProject/Controllers/ProductController.cs
+++ CodexMVCProject/Controllers/ProductController.cs
@@ -36,5 +36,58 @@ namespace CodexMVCProject.Controllers
                 return Json(new { success = false });
             }
         }
+
+        [HttpGet]
+        public IList<Product> Products()
+        {
+            var products = _dbContext.Products.ToList();
+            return products;
+        }
+
+        //Get
+        [HttpPost]
+        public IActionResult Delete(Product product)
+        {
+            if (product is null || product.Id == 0)
+            {
+                return NotFound();
+            }
+
+            var productDb = _dbContext.Products.Find(product.Id);
+
+            if (productDb is null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                _dbContext.Products.Remove(productDb);
+                _dbContext.SaveChanges();
+                return Json(new { success = true });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false });
+            }
+        }
+
+        //Post
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            DateTime utcDateTime = product.Date.ToUniversalTime();
+            product.Date = utcDateTime;
+            try
+            {
+                _dbContext.Products.Update(product);
+                _dbContext.SaveChanges();
+                return Json(new { success = true });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, Message = ex.Message });
+            }
+        }
     }
 }
